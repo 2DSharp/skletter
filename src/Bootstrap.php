3f44b4d52cb7d87f847a787c0dd04a8c330f0c44
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-07-02 02:47:33 +0530

    Route and build dependencies

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,9 +1,37 @@
 <?php
+/*
+ * This file is part of Skletter <https://github.com/2DSharp/Skletter>.
+ *
+ * (c) Dedipyaman Das <2d@twodee.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use Greentea\Core\Application;
 
+use function Skletter\getInjectorWithDependencies;
+use Skletter\RouteFactory;
+
 require_once __DIR__ . '../vendor/autoload.php';
 
-$dependencies = include_once(__DIR__ . '/Dependencies.php');
+$injector = getInjectorWithDependencies();
 
-$app = new Application($dependencies);
+
+$app = new Application($injector);
+
+try
+{
+    $request = $injector->make(\Symfony\Component\HttpFoundation\Request::class);
+    $routes = require_once(__DIR__.'/Routes.php');
+
+    $router = new RouteFactory($routes, $request, \Skletter\View\ErrorPages::class);
+    $router->buildPaths('Skletter\Controller\\', 'Skletter\View\\');
+
+    $app->run($request, $router);
+
+}
+catch (\Auryn\InjectionException $e) {
+}
+catch (\Skletter\Exception\InvalidErrorPage $e) {
+}
