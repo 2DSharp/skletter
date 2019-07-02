689898c6af6eb7653e4642f70696dd419254c542
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-07-02 20:54:43 +0530

    Make error pages load from template

--- src/View/ErrorPages.php
+++ src/View/ErrorPages.php
@@ -16,20 +16,57 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ErrorPages extends View implements ErrorPageView
 {
+    private $templating;
 
+    public function __construct(\Twig\Environment $twig)
+    {
+        $this->templating = $twig;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
     public function pageNotFound(Request $request) : Response
     {
-        return $this->respond($request, 'Page Not Found', 404);
+        $html = $this->createHTMLFromTemplate($this->templating, 'error.twig',
+            ['title' => 'Page not found!',
+                'message' => "Whoops! The page you requested either doesn't exist or you don't have the permission to view it."]);
+        return $this->respond($request, $html, 404);
     }
 
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
     public function methodNotAllowed(Request $request) : Response
     {
-        return $this->respond($request, 'Method not allowed', 401);
+        $html = $this->createHTMLFromTemplate($this->templating, 'error.twig',
+            ['title' => 'Invalid Method',
+                'message' => "The requested method is not allowed for this page."]);
+
+        return $this->respond($request, $html, 401);
     }
 
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
     public function internalError(Request $request) : Response
     {
-        return $this->respond($request, 'Internal Server error', 500);
+        $html = $this->createHTMLFromTemplate($this->templating, 'error.twig',
+            ['title' => 'Something went wrong',
+                'message' => "It's not you, it's us.\nWe ran into some problem and are fixing the problem as we speak."]);
+        return $this->respond($request, $html, 500);
     }
 
 }
