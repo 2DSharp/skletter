e3f3cbac9ecac2546494ef2c4d75ee05786c824d
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-07-02 20:51:16 +0530

    Add doc

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
-
+/*
+ * This file is part of Skletter <https://github.com/2DSharp/Skletter>.
+ *
+ * (c) Dedipyaman Das <2d@twodee.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 require_once(__DIR__ . '/../src/Bootstrap.php');
 ?>
