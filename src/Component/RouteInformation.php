50e366b05855ec97be7a1be4b288b8e5e3e4add6
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-07-09 21:54:09 +0530

    Use a separate VO implementing RouteVOInterface to extract route info

---
+++ src/Component/RouteInformation.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * This file is part of Skletter <https://github.com/2DSharp/Skletter>.
+ *
+ * (c) Dedipyaman Das <2d@twodee.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Skletter\Component;
+
+
+use Greentea\Component\RouteVOInterface;
+
+class RouteInformation implements RouteVOInterface
+{
+    /**
+     * @var string $controller - Controller class path
+     */
+    private $controller;
+    /**
+     * @var string $view - View class path
+     */
+    private $view;
+    /**
+     * @var string $method - method to be invoked in both controller and interface
+     */
+    private $method;
+
+    /**
+     * RouteInformation constructor.
+     * @param string $controller
+     * @param string $view
+     * @param string $method
+     */
+    public function __construct(string $controller, string $view, string $method)
+    {
+        $this->controller = $controller;
+        $this->view = $view;
+        $this->method = $method;
+    }
+
+    /**
+     * @return string
+     */
+    public function resolveController(): string
+    {
+        return $this->controller;
+    }
+
+    /**
+     * @return string
+     */
+    public function resolveView(): string
+    {
+        return $this->view;
+    }
+
+    /**
+     * @return string
+     */
+    public function resolveMethod(): string
+    {
+        return $this->method;
+    }
+}
