8f18862ed024adc6ca29a4a3c6417bc032d6475b
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-07-19 14:48:57 +0530

    Add DTO to pass data from controller to view

---
+++ src/Model/DTO/RegistrationState.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * This file is part of Skletter <https://github.com/2DSharp/Skletter>.
+ *
+ * (c) Dedipyaman Das <2d@twodee.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Skletter\Model\DTO;
+
+
+class RegistrationState
+{
+    private $error;
+    private $status;
+
+    /**
+     * @return mixed
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param mixed $error
+     */
+    public function setError($error): void
+    {
+        $this->error = $error;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+
+
+}
