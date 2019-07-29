391b904e53e083aa867c261b9b95288a8919bedf
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-07-29 22:30:29 +0530

    Use token as string and replace TokenPair

--- src/Model/Entity/CookieIdentity.php
+++ src/Model/Entity/CookieIdentity.php
@@ -11,74 +11,39 @@
 namespace Skletter\Model\Entity;
 
 use Skletter\Component\SecureTokenManager;
-use Skletter\Component\ValueObject\TokenKeyPair;
 use Skletter\Contract\Entity\Identity;
 use Skletter\Exception\InvalidCookie;
 
 class CookieIdentity implements Identity
 {
+
     /**
-     * @var string $hmacKey
+     * IdentityID
+     * @var int $id
      */
-    private $hmacKey;
     private $id;
     /**
      * @var string $token
      */
     private $token;
 
-    public static function createNew()
-    {
-        $pair = SecureTokenManager::generate();
-
-        $identity = new CookieIdentity();
-        $identity->setToken($pair->getToken());
-        $identity->setHmacKey($pair->getKey());
-        return $identity;
-    }
-
     /**
-     * @param TokenKeyPair $pair
+     * CookieIdentity constructor.
+     * @param string $token
      * @throws InvalidCookie
      */
-    public function createFromTokenKeyPair(TokenKeyPair $pair)
+    public function __construct(string $token)
     {
-        if (SecureTokenManager::isTampered($pair)) {
+        if (SecureTokenManager::isTampered($token)) {
             throw new InvalidCookie();
         }
-
-        $this->setHmacKey($pair->getKey());
-        $this->setToken($pair->getToken());
-    }
-    public function setId(int $id): void
-    {
-        $this->id = $id;
+        $this->token = $token;
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getIdentifier()
     {
         return $this->token;
-    }
-
-    /**
-     * @return string
-     */
-    public function getHmacKey(): string
-    {
-        return $this->hmacKey;
-    }
-
-    /**
-     * @param string $hmacKey
-     */
-    public function setHmacKey(string $hmacKey): void
-    {
-        $this->hmacKey = $hmacKey;
     }
 
     /**
@@ -89,11 +54,13 @@ class CookieIdentity implements Identity
         return $this->token;
     }
 
-    /**
-     * @param string $token
-     */
-    public function setToken(string $token): void
+    function setId(int $id): void
     {
-        $this->token = $token;
+        $this->id = $id;
+    }
+
+    function getId(): ?int
+    {
+        return $this->id;
     }
 }
