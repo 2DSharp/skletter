84006887f654817283f7de39b4aa88bf3c79433f
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-08-29 22:40:48 +0530

    Fix namespacing to maintain clean directory structure

--- src/Thrift/UserService.thrift
+++ src/Thrift/UserService.thrift
@@ -1,60 +1,25 @@
-namespace java com.skletter.service
+namespace java com.skletter.service.UserService
 namespace php Skletter.Model.RemoteService.UserService
 
-enum Status
-{
-  TEMP,
-  ACTIVE,
-  DEACTIVATED,
-  RECOVERY,
-  SUSPENDED
-}
-struct Profile
-{
-  1: required string username
-  2: required string name
-  3: optional string picture
-}
-
-struct User
-{
-  1: required i32 id
-  2: required string email
-  3: required string username
-  4: required Status status
-}
-
-exception UserExists
-{
-  1: string 	identifier
-  2: i16	errorCode
-}
-
-exception NonExistentUser
-{
-  1: string	identifier
-  2: i16	errorCode
- }
-
+include "Entity.thrift"
+include "Exception.thrift"
 typedef i32 int
 
-service UserWriteService
+service UserService
 {
-  void registerNew(1: string name, 2: string username, 3: string email) throws (1: UserExists exists),
-  void updateStatus(1: int id, 2: Status status),
-  void updateName(1: int id, 2: string name),
-  void updateUsername(1: int id, 2: string newUsername) throws (1: UserExists exists),
-  void updateProfilePicture(1: int id, 2: string newImage)
-  void deleteProfilePicture(1: int id),
-  void deactivate(1: int id),
-  void deleteUser(1: int id)
-}
+    void registerNew(1: Entity.User user, 2: Entity.Profile profile) throws (1: Exception.UserExists exists, 
+                                                                             2: Exception.ValidationError error),
+    void updateStatus(1: int id, 2: Entity.Status status),
+    void updateName(1: int id, 2: string name),
+    void updateUsername(1: int id, 2: string newUsername) throws (1: Exception.UserExists exists),
+    void updateProfilePicture(1: int id, 2: string newImage)
+    void deleteProfilePicture(1: int id),
+    void deactivate(1: int id),
+    void deleteUser(1: int id)
 
-service UserReadService
-{
-  User getUserByEmail(1: string email) throws (1: NonExistentUser nonexistentUser),
-  User getUserByID(1: int id) throws (1: NonExistentUser nonexistentUser),
-  User getUserByUsername(1: string username) throws (1: NonExistentUser nonexistentUser),
-  Profile getProfile(1: string username) throws (1: NonExistentUser nonexistentUser)
-  
+    Entity.User getUserByEmail(1: string email) throws (1: Exception.NonExistentUser nonexistentUser),
+    Entity.User getUserByID(1: int id) throws (1: Exception.NonExistentUser nonexistentUser),
+    Entity.User getUserByUsername(1: string username) throws (1: Exception.NonExistentUser nonexistentUser),
+    Entity.User getUserByIdentifier(1: string username) throws (1: Exception.NonExistentUser nonexistentUser),
+    Entity.Profile getProfile(1: string username) throws (1: Exception.NonExistentUser nonexistentUser)
 }
