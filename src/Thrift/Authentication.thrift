929def5c273a80a593947f611a614a1615837fd1
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-08-30 00:42:41 +0530

    Add DTO to return results from remote services

--- src/Thrift/Authentication.thrift
+++ src/Thrift/Authentication.thrift
@@ -2,6 +2,7 @@ namespace java com.skletter.service.Authentication
 namespace php Skletter.Model.RemoteService.Authentication
 
 include "Exception.thrift"
+include "DTO.thrift"
 
 typedef i32 int
 
@@ -14,10 +15,10 @@ service Authentication
     void createNonceTokenIdentity(1: int userId, 2: DateTime expiresOn, 3: string format),
     void createNoncePinIdentity(1: int userId, 2: DateTime expiresOn, 3: string format),
 
-    bool authenticatePassword(1: int userId, 2: string password) throws (1: Exception.NonExistentUser nonexistentUser),
-    bool authenticateCookie(1: string cookie, 2: string userAgent) throws (1: Exception.NonExistentUser nonexistentUser),
-    bool authenticateNonceToken(1: int userId, 2: string token) throws (1: Exception.NonExistentUser nonexistentUser),
-    bool authenticateNoncePin(1: int userId, 2: i16 pin) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.AuthResult authenticatePassword(1: int userId, 2: string password) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.AuthResult authenticateCookie(1: string cookie, 2: string userAgent) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.AuthResult authenticateNonceToken(1: int userId, 2: string token) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.AuthResult authenticateNoncePin(1: int userId, 2: i16 pin) throws (1: Exception.NonExistentUser nonexistentUser),
 
     void updateStandardIdentity(1: int userId, 2: string password),
     void invalidateCookie(1: string cookie),
---
+++ src/Thrift/DTO.thrift
@@ -0,0 +1,9 @@
+namespace java com.skletter.Model.DTO
+namespace php Skletter.Model.RemoteService.DTO
+
+
+struct AuthResult
+{
+  1: required bool success
+  2: optional string error
+}
