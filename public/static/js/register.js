76d2d6c1630310cf6a586e2d6942b53e8aa3049f
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-09-12 13:41:03 +0530

    Add new per field basis error display system

--- public/static/js/register.js
+++ public/static/js/register.js
@@ -1,10 +1,10 @@
 function register(event, actionLocation) {
     event.preventDefault();
     document.getElementById('reg-processor').style.display = "block";
-    let error_box = document.getElementById('reg-error-box');
     let loader = document.getElementById('reg-loader');
 
-    error_box.style.display = "none";
+    toggle("error-box", "display", "none");
+    resetErroredInput();
     loader.style.display = "initial";
 
     axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -15,6 +15,7 @@ function register(event, actionLocation) {
     form.append('username', document.getElementById('username').value);
     form.append('password', document.getElementById('password').value);
 
+
     axios.post(actionLocation, form)
         .then(function (response) {
             if (response.data['status'] === 'success') {
@@ -22,8 +23,12 @@ function register(event, actionLocation) {
             }
             else {
                 loader.style.display = "none";
-                error_box.style.display = "block";
-                error_box.innerText = response.data["error"];
+                let errors = response.data["errors"];
+
+                for (let key in errors) {
+                    displayError(key, errors[key]);
+                }
+
                 console.log(response)
             }
         })
@@ -31,3 +36,31 @@ function register(event, actionLocation) {
             console.log(error);
         });
 }
+
+function displayError(key, error) {
+    console.log(key);
+    console.log(error);
+    let box = document.getElementById(key + "-errbox");
+
+    document.getElementById(key).className += " errored-input";
+    box.style.display = "block";
+    box.innerText = error;
+}
+
+function toggle(className, property, displayState) {
+    let elements = document.getElementsByClassName(className);
+
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style = property + ":" + displayState;
+    }
+}
+
+function resetErroredInput() {
+    let elements = document.getElementsByClassName("errored-input");
+
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.remove("errored-input");
+    }
+
+}
+
