930f7e8a43a7fab97c19927d899f57c6ba9d4ca0
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-09-16 01:12:46 +0530

    Merge authentication service with User service

--- src/Thrift/UserService.thrift
+++ src/Thrift/UserService.thrift
@@ -1,15 +1,17 @@
-namespace java com.skletter.Service
+namespace java com.skletter
 namespace php Skletter.Model.RemoteService.UserService
 
-include "Entity.thrift"
+include "DTO.thrift"
 include "Exception.thrift"
 typedef i32 int
 
 service UserService
 {
-    void registerNew(1: Entity.User user, 2: Entity.Profile profile) throws (1: Exception.UserExists exists, 
-                                                                             2: Exception.ValidationError error),
-    void updateStatus(1: int id, 2: Entity.Status status),
+    int registerNew(1: DTO.UserDTO user) throws (1: Exception.UserExists exists,
+                                                 2: Exception.ValidationError error,
+                                                 3: Exception.NullDTOException nullDTOException),
+    void confirmAccountViaToken(1: int id, 2: string token),
+    void confirmAccountViaPIN(1: int id, 2: i16 pin),
     void updateName(1: int id, 2: string name),
     void updateUsername(1: int id, 2: string newUsername) throws (1: Exception.UserExists exists),
     void updateProfilePicture(1: int id, 2: string newImage)
@@ -17,9 +19,14 @@ service UserService
     void deactivate(1: int id),
     void deleteUser(1: int id)
 
-    Entity.User getUserByEmail(1: string email) throws (1: Exception.NonExistentUser nonexistentUser),
-    Entity.User getUserByID(1: int id) throws (1: Exception.NonExistentUser nonexistentUser),
-    Entity.User getUserByUsername(1: string username) throws (1: Exception.NonExistentUser nonexistentUser),
-    Entity.User getUserByIdentifier(1: string username) throws (1: Exception.NonExistentUser nonexistentUser),
-    Entity.Profile getProfile(1: string username) throws (1: Exception.NonExistentUser nonexistentUser)
+    DTO.UserDTO getUserByEmail(1: string email) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.UserDTO getUserByID(1: int id) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.UserDTO getUserByUsername(1: string username) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.UserDTO getUserByIdentifier(1: string username) throws (1: Exception.NonExistentUser nonexistentUser),
+
+    DTO.UserDTO loginWithPassword(1: string identifier, 2: string password) throws (1: Exception.NonExistentUser nonexistentUser,
+                                                                                2: Exception.PasswordMismatch mismatch),
+    DTO.AuthResult authenticateCookie(1: string cookie, 2: string userAgent) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.AuthResult authenticateNonceToken(1: int userId, 2: string token) throws (1: Exception.NonExistentUser nonexistentUser),
+    DTO.AuthResult authenticateNoncePin(1: int userId, 2: i16 pin) throws (1: Exception.NonExistentUser nonexistentUser),
 }
