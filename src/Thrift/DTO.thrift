2871ade00320bf2e87b1110e8a9bcbe50ddeca3c
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-09-16 01:13:13 +0530

    DTO now replaces entity

--- src/Thrift/DTO.thrift
+++ src/Thrift/DTO.thrift
@@ -1,4 +1,4 @@
-namespace java com.skletter.Model.DTO
+namespace java com.skletter.DTO
 namespace php Skletter.Model.RemoteService.DTO
 
 
@@ -6,4 +6,24 @@ struct AuthResult
 {
   1: required bool success
   2: optional string error
+}
+
+enum Status
+{
+  TEMP,
+  ACTIVE,
+  DEACTIVATED,
+  RECOVERY,
+  SUSPENDED
+}
+
+struct UserDTO
+{
+  1: optional i32 id
+  2: required string name
+  3: required string email
+  4: required string username
+  5: optional Status status
+  6: optional string password
+  7: optional string ipAddr
 }
