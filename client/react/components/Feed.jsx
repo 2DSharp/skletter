1e13195a7ac0c91d0bb6cafa03e2e07272b572f8
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-12-01 20:51:26 +0530

    Load data from a file temporarily

--- client/react/components/Feed.jsx
+++ client/react/components/Feed.jsx
@@ -1,45 +1,22 @@
 import React from "react";
 import PostCard from "./PostCard";
 
-'use strict';
-
 class Feed extends React.Component {
-
     componentDidMount() {
-        setTimeout(function () {
-            this.setState({
-                isLoaded: true,
-                posts: [
-                    {
-                        id: 1,
-                        name: "Dedipyaman Das",
-                        username: "@twodee23",
-                        profile_picture: "http://localhost/static/img/test.jpg",
-                        subject: "Lorem ipsum dolor sit amet, consectetur adipiscing",
-                        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore " +
-                            "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
-                            "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat " +
-                            "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim" +
-                            " id est laborum."
-                    },
-                    {
-                        id: 2,
-                        name: "John Doe",
-                        username: "@jdoe",
-                        profile_picture: "http://localhost/static/img/test.jpg",
-                        subject: "Sed ut perspiciatis unde omnis iste natus",
-                        content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium," +
-                            " totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta " +
-                            "sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia" +
-                            " consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui" +
-                            " dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora " +
-                            "incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
-                    }
 
-                ]
+        axios.get('https://gist.githubusercontent.com/2DSharp/86fe64a377af70d1d9ee09b82dbeceaa/raw/2685ec1a373ef285bde9d1d2e7abe3171a24f546/test.json')
+            .then(function (response) {
+                this.setState({
+                    isLoaded: true,
+                    posts: response.data.posts
+                });
+            }.bind(this))
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
             });
-
-        }.bind(this), 3000);
     }
 
     state = {
@@ -58,11 +35,15 @@ class Feed extends React.Component {
                 </div>
             );
         } else {
-            return (
-                <div className="post-card">
-                    <div className="skeleton-feed"/>
-                </div>
-            );
+            let rows = [];
+            for (let i = 0; i < 3; i++) {
+                rows.push(
+                    <div key={i} className="post-card">
+                        <div className="skeleton-feed"/>
+                    </div>
+                );
+            }
+            return <div>{rows}</div>;
         }
     }
 }
