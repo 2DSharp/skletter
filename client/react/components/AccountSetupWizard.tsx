1774e8262a3a506b1bbecbc634bf90b5f3ac2535
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-12-26 20:53:31 +0530

    Split steps into smaller components

--- client/react/components/AccountSetupWizard.tsx
+++ client/react/components/AccountSetupWizard.tsx
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import Button from "./Button";
-import ImageUploader from "./ImageUploader";
-import Axios, {AxiosResponse} from "axios";
+import UpdateDPStep from "./AccountSetupWizard/UpdateDPStep";
 
 export interface AccountSetupWizardProps {
     step: number;
@@ -10,78 +9,19 @@ export interface AccountSetupWizardProps {
 class AccountSetupWizard extends Component<AccountSetupWizardProps, {}> {
     constructor(props: AccountSetupWizardProps) {
         super(props);
-        this.updatePicture = this.updatePicture.bind(this);
-        this.fetchAndAddPicture = this.fetchAndAddPicture.bind(this);
     }
 
     state = {
         step: 1,
-        pic: "http://localhost/static/upload/default.png"
     };
 
     componentDidMount() {
-        this.updatePicture();
-    }
-
-    uploadPicturePrompt() {
-        return (
-            <div style={{textAlign: "center"}}>
-                <h1>Let's get you up to speed</h1>
-                <h3 className="dialog-subhead">Add a profile picture</h3>
-                <div
-                    style={{
-                        backgroundImage: "url(" + this.state.pic + ")",
-                        display: "inline-block",
-                        width: "128px",
-                        height: "128px"
-                    }}
-                    className="profile-image"
-                />
-                <div>
-                    <div className="spacer medium"/>
-                    <ImageUploader
-                        onUpdate={this.updatePicture}
-                        placeholder="Uploading Profile Picture..."
-                        endpoint={process.env.API_URL + "/uploadPicture"}
-                    />
-                </div>
-                <div className="spacer large"/>
-                <div>
-                    A profile picture is associated with your identity making you unique.
-                </div>
-            </div>
-        );
-    }
-
-    updatePicture() {
-        Axios.get(process.env.API_URL + "/getCurrentUserDetails")
-            .then(
-                function (response: AxiosResponse) {
-                    const username = response.data.username;
-                    this.fetchAndAddPicture(username);
-                }.bind(this)
-            )
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    fetchAndAddPicture(username: string) {
-        Axios.get(process.env.API_URL + "/getProfilePicture?username=" + username)
-            .then(
-                function (response: AxiosResponse) {
-                    this.setState({pic: response.data.url});
-                }.bind(this)
-            )
-            .catch(function (error) {
-                console.log(error);
-            });
     }
 
     renderStep(step: number) {
         switch (step) {
             case 1:
-                return this.uploadPicturePrompt();
+                return <UpdateDPStep/>;
             case 2:
                 return null;
         }
---
+++ client/react/components/AccountSetupWizard/UpdateDPStep.tsx
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from "react";
+import ImageUploader from "../ImageUploader";
+import Axios, {AxiosResponse} from "axios";
+
+const UpdateDPStep = () => {
+    const [picture, setPicture] = useState(
+        "http://localhost/static/upload/default.png"
+    );
+    const fetchAndAddPicture = (username: string) => {
+        Axios.get(
+            process.env.API_URL +
+            "/getProfilePicture?username=" +
+            username +
+            "&variant=big"
+        )
+            .then(function (response: AxiosResponse) {
+                setPicture(response.data.url);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+    const updatePicture = () => {
+        Axios.get(process.env.API_URL + "/getCurrentUserDetails")
+            .then(function (response: AxiosResponse) {
+                fetchAndAddPicture(response.data.username);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
+    useEffect(() => updatePicture());
+    return (
+        <div style={{textAlign: "center"}}>
+            <h1>Let's get you up to speed</h1>
+            <h3 className="dialog-subhead">Add a profile picture</h3>
+            <div
+                style={{
+                    backgroundImage: "url(" + picture + ")",
+                    display: "inline-block",
+                    width: "128px",
+                    height: "128px"
+                }}
+                className="profile-image"
+            />
+            <div>
+                <div className="spacer medium"/>
+                <ImageUploader
+                    onUpdate={updatePicture}
+                    placeholder="Uploading Profile Picture..."
+                    endpoint={process.env.API_URL + "/uploadPicture"}
+                />
+            </div>
+            <div className="spacer large"/>
+            <div>
+                A profile picture is associated with your identity making you unique.
+            </div>
+        </div>
+    );
+};
+
+export default UpdateDPStep;
