4c05d9a9f6fdb71aabdf8c94dce3c13884c95848
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-01-13 01:38:28 +0530

    Trim and fetch data from the timeline fetch API

--- client/react/components/Feed.tsx
+++ client/react/components/Feed.tsx
@@ -9,11 +9,12 @@ export interface FeedState {
 
 class Feed extends React.Component<{}, FeedState> {
     componentDidMount() {
-        Axios.get('https://gist.githubusercontent.com/2DSharp/86fe64a377af70d1d9ee09b82dbeceaa/raw/2685ec1a373ef285bde9d1d2e7abe3171a24f546/test.json')
+
+        Axios.get(process.env.API_URL + "/fetchTimeline")
             .then(function (response: AxiosResponse) {
                 this.setState({
                     isLoaded: true,
-                    posts: response.data.posts
+                    posts: response.data
                 });
             }.bind(this))
             .catch(function (error) {
--- client/react/components/Post/PostCard.tsx
+++ client/react/components/Post/PostCard.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import ProfilePicture, {ProfilePictureVariant} from "../ProfilePicture";
+import {ContentBlock, convertFromRaw, Editor, EditorState} from "draft-js";
 
 export interface PostCard {
     data: any
 }
 
+function trimEmptyBlocks(contentBlock: ContentBlock) {
+    const length = contentBlock.getLength();
+    if (length === 0) {
+        return 'empty-block'
+    }
+}
 const PostCard: React.FunctionComponent<PostCard> = (props: PostCard) => {
+    const editorState = EditorState.createWithContent(convertFromRaw(JSON.parse(props.data.content)));
     return (
         <div>
             <div className="post-card">
                 <ProfilePicture username={props.data.username} variant={ProfilePictureVariant.SMALL}/>
                 <div className="post-text">
-                    <div className="subject">{props.data.subject}</div>
+                    <div className="subject">{props.data.title}</div>
                     <div className="byline">
-                        <div className="profile-meta"><b>{props.data.name} </b> ({props.data.username})</div>
+                        <div className="profile-meta"><b>{props.data.composerName} </b> ({props.data.username})</div>
                     </div>
                     <div className="post-content">
-                        <p>
-                            {props.data.content}
-                        </p>
+                        <Editor blockStyleFn={trimEmptyBlocks} onChange={() => console.log()} editorState={editorState}
+                                readOnly/>
                     </div>
                 </div>
                 <div className="post-action">
