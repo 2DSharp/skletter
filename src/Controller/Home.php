9014fb45ff6663e9aaaa5f4c7e01d40e5b659937
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-02-26 20:05:17 +0530

    Allow cookie logins

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -12,17 +12,43 @@ namespace Skletter\Controller;
 
 
 use Skletter\Component\Core\Controller;
+use Skletter\Exception\InvalidCookie;
+use Skletter\Model\LocalService\CookieManager;
+use Skletter\Model\LocalService\SessionManager;
+use Skletter\Model\Mediator\AccountService;
 use Symfony\Component\HttpFoundation\Request;
 
 class Home implements Controller
 {
-    private $loginManager;
-    private $map;
+    use ControllerTrait;
+    /**
+     * @var SessionManager
+     */
+    private SessionManager $sessionManager;
+    /**
+     * @var AccountService
+     */
+    private AccountService $accountService;
 
-    public function __construct(LoginManager $loginManager, IdentityMap $map)
+    public function __construct(SessionManager $sessionManager, AccountService $accountService)
     {
-        $this->loginManager = $loginManager;
-        $this->map = $map;
+        $this->sessionManager = $sessionManager;
+        $this->accountService = $accountService;
+    }
+
+    public function main(Request $request)
+    {
+        try {
+
+            if (!$this->sessionManager->isLoggedIn()) {
+                $token = $request->cookies->get($_ENV['PERSISTENCE_COOKIE'], 'none');
+                $params = ['User-Agent' => $request->headers->get('User-Agent')];
+                $cookieDTO = CookieManager::getLoginCookie($token, $params);
+                $this->accountService->loginWithCookie($cookieDTO, $params);
+            }
+        } catch (InvalidCookie $e) {
+            //log this
+        }
     }
 
     public function handleRequest(Request $request, string $method): void
